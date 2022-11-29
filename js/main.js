1ef94c6b4045eb34a7c4ba06e067711cc0b923bd
Vsevolod Babansky <sevab200308@gmail.com>
2022-11-29 23:10:41 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,44 @@
+const steps = Array.from(document.querySelectorAll('form .step'));
+const nextBtn = document.querySelectorAll('form .next-btn');
+const prevBtn = document.querySelectorAll('form .previous-btn');
+const form = document.querySelector('form');
+
+function changeStep(btn) {
+    let index = 0;
+    const active = document.querySelector('form .step.active');
+    index = steps.indexOf(active);
+    steps[index].classList.remove('active');
+    if(btn === 'next') {
+        index ++;
+    }else if(btn === 'prev') {
+        index --;
+    }
+
+    steps[index].classList.add('active')
+}
+
+
+nextBtn.forEach(button => {
+    button.addEventListener('click', () => {
+        changeStep('next');
+    })
+})
+prevBtn.forEach(button => {
+    button.addEventListener('click', () => {
+        changeStep('prev')
+    })
+})
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const inputs = [];
+    form.querySelectorAll('input').forEach(input => {
+        const {name, value} = input;
+        inputs.push({name, value})
+    })
+    form.reset();
+    let index = 0;
+    const active = document.querySelector('form .step.active');
+    index = steps.indexOf(active);
+    steps[index].classList.remove('active');
+    steps[2].classList.add('active')
+})
